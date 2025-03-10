ff9bdae183d23de9a76574e5eca11423d40de224
AndrewRBurge <burgean@sheridancollege.ca>
2025-03-09 21:47:58 -0400

    set up test project
    - created test project

---
+++ CSCN71020_A2/MSTest/MSTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MSTest
+{
+	TEST_CLASS(MSTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
